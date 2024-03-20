069426e20a92c37e40f580c57feb9f8404598432
eshaaaa2512 <157003271+eshaaaa2512@users.noreply.github.com>
2024-03-20 15:13:25 +0530

    [ESHA] ADD : creating a trigger for updating the data for new cases

---
+++ TRIGGER_NEW_CASE.sql
@@ -0,0 +1,14 @@
+USE COVID_DB;
+SELECT * FROM country_wise_latest;
+CREATE TRIGGER TRIG_NEW_CASES
+ON COUNTRY_WISE_LATEST
+FOR
+UPDATE
+AS 
+BEGIN
+      PRINT'A NEW CASE HAS BEEN UPDATED'
+END;
+
+UPDATE country_wise_latest
+SET New_cases = 10
+WHERE Country_Region = 'Western Sahara';
